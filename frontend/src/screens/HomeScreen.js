cc28c3312a80ed3c8fc292ec51136d25fa2182e1
Woo Sung Kang <geniusraban@gmail.com>
2022-08-19 00:28:47 +0900

    fetch products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
-import data from "../data";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+      const fetchData = async () => {
+        const result = await axios.get('/api/products'); // by calling axios.get method, send ajax request to this url and store it in var 
+        setProducts(result.data) //result.data is data in backend
+      };
+      fetchData();
+    },[])
+
     return (<div>
         <h1>Featured Products</h1>
           <div className="products">
-            {data.products.map(product => (
+            {products.map((product) => (
               <div className="product" key={product.slug}>
                 <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name} />
