9db74e770f37c87cda40be92515436e1926b3473
Woo Sung Kang <geniusraban@gmail.com>
2022-08-20 23:26:29 +0900

    create react context

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import axios from "axios";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
+import { Store } from "../Store";
 
 const reducer= (state, action) => { //by using reducer hook, we can depart state logic with component 
     switch(action.type){
@@ -45,6 +46,11 @@ function ProductScreen(){
         };
         fetchData();
       },[slug])
+      
+      const {state, dispatch: ctxDispatch} = useContext(Store);
+      const addToCartHandler = () => {
+        ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity: 1}})
+      }
 
     return (
         loading ? (<LoadingBox />)
@@ -105,7 +111,7 @@ function ProductScreen(){
                                 {product.countInStock > 0 && (
                                     <ListGroup.Item>
                                         <div className="d-grid">
-                                            <Button variant="primary">
+                                            <Button onClick={addToCartHandler}variant="primary">
                                                 Add to Cart
                                             </Button>
                                         </div>
