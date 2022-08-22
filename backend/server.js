1a3100157336f50480954207d8596f5810cff3c6
Woo Sung Kang <geniusraban@gmail.com>
2022-08-22 14:29:34 +0900

    Improve add to cart

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,14 @@ app.get('/api/products/slug/:slug', (req, res) => {
     }
 }) //by colon slug, we can get slug that user enterd
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message : 'Product Not Found'});
+    }    
+})
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -48,11 +48,19 @@ function ProductScreen(){
       },[slug])
       
       const {state, dispatch: ctxDispatch} = useContext(Store);
-      const addToCartHandler = () => {
-        ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity: 1}})
+      const {cart} = state;
+      const addToCartHandler = async () => {
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if (data.countInStock < quantity){
+            window.alert('Sorry. Product is out of stock');
+            return;
+        }
+        ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity}})
       }
 
-    return (
+    return ( 
         loading ? (<LoadingBox />)
         :
         error ? (<MessageBox variant="danger">{error}</MessageBox>) 
